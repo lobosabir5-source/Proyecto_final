a1711c8fdf657c2529dfea089fde341a6bb91896
lobosabir5-source <tu_correo@gmail.com>
2026-09-04 22:22:12 -0400

    Proyecto Subido

---
+++ src/test/java/com/Golds_Gym/Gimnasio/GimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Golds_Gym.Gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
