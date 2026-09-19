b358122e9098bfb114d06bbd593dfb39acb0b97c
xSaBiRx009 <lobosabir5@gmail.com>
2026-09-18 21:35:38 -0400

    Home creado y conectado al login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,19 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-	{
-		path: 'login',
-		loadComponent: () => import('./pages/login/login').then(m => m.Login)
-	},
-	{
-		path: 'dashboard',
-		canActivate: [authGuard],
-		loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
-	},
-	{ path: '', pathMatch: 'full', redirectTo: 'login' },
-	{ path: '**', redirectTo: 'login' }
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then(m => m.Login)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+  },
+  { path: '**', redirectTo: '' }
 ];
